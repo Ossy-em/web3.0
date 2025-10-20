7dd84efba52a78b881e29fb43a509f06128cc703
Ossy-em <emosinachi@gmail.com>
2025-10-20 22:14:22 +0100

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { TransactionsProvider } from "./context/TransactionContext";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <TransactionsProvider>
   <StrictMode>
     <App />
   </StrictMode>,
+  </TransactionsProvider>
 )
